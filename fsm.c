7d411ceaecdfa0470ca13f16dcd46ff3f968d633
pietro_baroni <baroni.pietro11@gmail.com>
2025-12-05 22:09:48 +0100

    crated file fsm.h and fsm.c

---
+++ fsm.c
@@ -0,0 +1,151 @@
+#include "fsm.h"
+
+void boot(void);
+void door_locked(void);
+void insert_pin(void);
+void open_door(void);
+void wait_RFID(void);
+void admin_menu(void);
+void wrong_pin(void);
+void last_pin(void);
+void block_access(void);
+void wait_reset_door(void);
+
+
+// ------------------------------------------------------ //
+//All these functions have to be implemented, they are like this just to compile
+void boot(void){
+    printf("A");
+}
+void door_locked(void){
+    printf("A");
+}
+void insert_pin(void){
+    printf("A");
+}
+void open_door(void){
+    printf("A");
+}
+void wait_RFID(void){
+    printf("A");
+}
+void admin_menu(void){
+    printf("A");
+}
+void wrong_pin(void){
+    printf("A");
+}
+void last_pin(void){
+    printf("A");
+}
+void block_access(void){
+    printf("A");
+}
+void wait_reset_door(void){
+    printf("A");
+}
+
+// ------------------------------------------------ //
+
+State_t cur_state = STATE_BOOT;
+
+StateMachine_t fsm[] = {
+     {STATE_BOOT, fn_BOOT},
+     {STATE_DOOR_LOCKED, fn_DOOR_LOCKED},
+     {STATE_INSERT_PIN, fn_INSERT_PIN},
+     {STATE_OPEN_DOOR, fn_OPEN_DOOR},
+     {STATE_WAIT_RFID, fn_WAIT_RFID},
+     {STATE_ADMIN_MENU, fn_ADMIN_MENU},
+     {STATE_WRONG_PIN, fn_WRONG_PIN},
+     {STATE_LAST_PIN, fn_LAST_PIN},
+     {STATE_BLOCK_ACCESS, fn_BLOCK_ACCESS},
+     {STATE_WAIT_RESET_DOOR, fn_WAIT_RESET_DOOR},
+};
+
+// -----------------------------------------------//
+// Implementation of the state's functions
+
+void fn_BOOT(void){
+    printf("Boot \n");
+    boot();
+    cur_state = STATE_DOOR_LOCKED;
+}
+
+void fn_DOOR_LOCKED(void){
+    printf("Door Locked \n");
+    door_locked();
+    cur_state = STATE_INSERT_PIN;
+}
+
+void fn_INSERT_PIN(void){
+    printf("Insert Pin \n");
+    insert_pin();
+    //if pin_user is correct
+    //  cur_state = STATE_OPEN_DOOR;
+    //else if pin_admin is correct
+    //  cur_state = STATE_WAIT_RFID
+    //else
+    //  cur_state = STATE_WRONG_PIN
+}
+
+void fn_OPEN_DOOR(void){
+    printf("Door open \n");
+    open_door();
+    cur_state = STATE_DOOR_LOCKED;
+}
+
+void fn_WAIT_RFID(void){
+    printf("Wait for RFID \n");
+    wait_RFID();
+    cur_state = STATE_ADMIN_MENU;
+}
+
+void fn_ADMIN_MENU(void){
+    printf("Show admin menu \n");
+    admin_menu();
+    //cur_state = other states for each function
+}
+
+void fn_WRONG_PIN(void){
+    printf("Wrong pin \n");
+    wrong_pin(); //show an error message on display
+    //if error_counter<3
+    //  cur_state = STATE_INSERT_PIN;
+    //else if error_counter=3
+    //  cur_state = STATE_LAST_PIN
+}
+
+void fn_LAST_PIN(void){
+    printf("Last possibility for right pin \n");
+    last_pin();
+    //if pin_user is correct
+    //  cur_state = STATE_OPEN_DOOR;
+    //else
+    //  cur_state = STATE_BLOCK_ACCESS
+}
+
+void fn_BLOCK_ACCESS(void){
+    printf("Access blocked \n");
+    block_access();
+    cur_state = STATE_WAIT_RESET_DOOR;
+}
+
+void fn_WAIT_RESET_DOOR(void){
+    printf("Wait door to be reset \n");
+    wait_reset_door();
+    //if telegram or RFID
+    //  cur_state = STATE_INSERT_PIN;
+}
+
+// ---------------------------------------------//
+
+// Function to run in the main
+void FSM_Run(void)
+{
+    if(cur_state < NUM_STATES){
+        (*fsm[cur_state].state_function)();
+    }
+    else{
+        // Gestione errore stato non valido
+    }
+}
---
+++ fsm.h
@@ -0,0 +1,47 @@
+#ifndef FSM_H
+#define FSM_H
+
+#include "msp.h"
+#include <stdio.h>
+#include <ti/devices/msp432p4xx/driverlib/driverlib.h>
+
+/*
+ * This header file defines the states of the system
+ */
+
+typedef enum{
+    STATE_BOOT,
+    STATE_DOOR_LOCKED,
+    STATE_INSERT_PIN,
+    STATE_OPEN_DOOR,
+    STATE_WAIT_RFID, //after the admin insert the pin, wait for the RFID to access menu
+                     //we should consider the case in which the RFID doesn't arrive (maybe a timer)
+    STATE_ADMIN_MENU, //there must be other states for each functionality
+    STATE_WRONG_PIN,
+    STATE_LAST_PIN,
+    STATE_BLOCK_ACCESS,
+    STATE_WAIT_RESET_DOOR, //after this return to STATE_DOOR_LOCKED o STATE_INSERT_PIN
+    NUM_STATES
+}State_t;
+
+typedef struct{
+    State_t state;
+    void (*state_function)(void);
+} StateMachine_t;
+
+void fn_BOOT(void);
+void fn_DOOR_LOCKED(void);
+void fn_INSERT_PIN(void);
+void fn_OPEN_DOOR(void);
+void fn_WAIT_RFID(void);
+void fn_ADMIN_MENU(void);
+void fn_WRONG_PIN(void);
+void fn_LAST_PIN(void);
+void fn_BLOCK_ACCESS(void);
+void fn_WAIT_RESET_DOOR(void);
+
+
+// Function to run in the loop
+void FSM_Run(void);
+
+#endif
