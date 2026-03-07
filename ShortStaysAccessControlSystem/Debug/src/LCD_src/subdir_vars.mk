296c27bfeb115da590fc289f991864e925ed4abd
Biasio <alessandro.biasioli@gmail.com>
2026-03-07 15:58:08 +0100

    Imported some metadata for ccs

--- ShortStaysAccessControlSystem/Debug/src/LCD_src/subdir_vars.mk
+++ ShortStaysAccessControlSystem/Debug/src/LCD_src/subdir_vars.mk
@@ -4,8 +4,8 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
-/Users/biasio/Documents/Documents_Nextcloud/UNITN/EMBEDDED/dev-biasio/workspace_1/simplelink_msp432p4_sdk_3_40_01_02/examples/nortos/MSP_EXP432P401R/demos/boostxl_edumkii_lightsensor_msp432p401r/LcdDriver/Crystalfontz128x128_ST7735.c \
-/Users/biasio/Documents/Documents_Nextcloud/UNITN/EMBEDDED/dev-biasio/workspace_1/simplelink_msp432p4_sdk_3_40_01_02/examples/nortos/MSP_EXP432P401R/demos/boostxl_edumkii_lightsensor_msp432p401r/LcdDriver/HAL_MSP_EXP432P401R_Crystalfontz128x128_ST7735.c 
+/Users/biasio/Documents/Documents_Nextcloud/UNITN/EMBEDDED/dev_biasio/simplelink_msp432p4_sdk_3_40_01_02/examples/nortos/MSP_EXP432P401R/demos/boostxl_edumkii_lightsensor_msp432p401r/LcdDriver/Crystalfontz128x128_ST7735.c \
+/Users/biasio/Documents/Documents_Nextcloud/UNITN/EMBEDDED/dev_biasio/simplelink_msp432p4_sdk_3_40_01_02/examples/nortos/MSP_EXP432P401R/demos/boostxl_edumkii_lightsensor_msp432p401r/LcdDriver/HAL_MSP_EXP432P401R_Crystalfontz128x128_ST7735.c 
 
 C_DEPS += \
 ./src/LCD_src/Crystalfontz128x128_ST7735.d \
@@ -24,7 +24,7 @@ C_DEPS__QUOTED += \
 "src/LCD_src/HAL_MSP_EXP432P401R_Crystalfontz128x128_ST7735.d" 
 
 C_SRCS__QUOTED += \
-"/Users/biasio/Documents/Documents_Nextcloud/UNITN/EMBEDDED/dev-biasio/workspace_1/simplelink_msp432p4_sdk_3_40_01_02/examples/nortos/MSP_EXP432P401R/demos/boostxl_edumkii_lightsensor_msp432p401r/LcdDriver/Crystalfontz128x128_ST7735.c" \
-"/Users/biasio/Documents/Documents_Nextcloud/UNITN/EMBEDDED/dev-biasio/workspace_1/simplelink_msp432p4_sdk_3_40_01_02/examples/nortos/MSP_EXP432P401R/demos/boostxl_edumkii_lightsensor_msp432p401r/LcdDriver/HAL_MSP_EXP432P401R_Crystalfontz128x128_ST7735.c" 
+"/Users/biasio/Documents/Documents_Nextcloud/UNITN/EMBEDDED/dev_biasio/simplelink_msp432p4_sdk_3_40_01_02/examples/nortos/MSP_EXP432P401R/demos/boostxl_edumkii_lightsensor_msp432p401r/LcdDriver/Crystalfontz128x128_ST7735.c" \
+"/Users/biasio/Documents/Documents_Nextcloud/UNITN/EMBEDDED/dev_biasio/simplelink_msp432p4_sdk_3_40_01_02/examples/nortos/MSP_EXP432P401R/demos/boostxl_edumkii_lightsensor_msp432p401r/LcdDriver/HAL_MSP_EXP432P401R_Crystalfontz128x128_ST7735.c" 
 
 
