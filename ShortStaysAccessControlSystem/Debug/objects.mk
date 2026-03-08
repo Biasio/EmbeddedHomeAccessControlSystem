cb678cce0a18b22753c6e9f6b107ae582431e2b2
Biasio <alessandro.biasioli@gmail.com>
2026-03-08 10:49:59 +0100

    Deleted some compiled files

--- ShortStaysAccessControlSystem/Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -llibc.a -lgrlib.a -l"/Users/biasio/Documents/Documents_Nextcloud/UNITN/EMBEDDED/dev_biasio/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/ccs/msp432p4xx_driverlib.lib"
-
