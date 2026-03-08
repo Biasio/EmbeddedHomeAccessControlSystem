1cd3486cfab1fc1af5ffd9694cd8a416377dadb7
Biasio <alessandro.biasioli@gmail.com>
2026-03-08 10:37:59 +0100

    Renamed to main.c

--- ShortStaysAccessControlSystem/Debug/src/subdir_vars.mk
+++ ShortStaysAccessControlSystem/Debug/src/subdir_vars.mk
@@ -7,54 +7,54 @@ C_SRCS += \
 ../src/HAL_I2C.c \
 ../src/HAL_OPT3001.c \
 ../src/display.c \
-../src/door_lock_system.c \
 ../src/fsm.c \
 ../src/joystick.c \
+../src/main.c \
 ../src/push_button.c 
 
 C_DEPS += \
 ./src/HAL_I2C.d \
 ./src/HAL_OPT3001.d \
 ./src/display.d \
-./src/door_lock_system.d \
 ./src/fsm.d \
 ./src/joystick.d \
+./src/main.d \
 ./src/push_button.d 
 
 OBJS += \
 ./src/HAL_I2C.obj \
 ./src/HAL_OPT3001.obj \
 ./src/display.obj \
-./src/door_lock_system.obj \
 ./src/fsm.obj \
 ./src/joystick.obj \
+./src/main.obj \
 ./src/push_button.obj 
 
 OBJS__QUOTED += \
 "src/HAL_I2C.obj" \
 "src/HAL_OPT3001.obj" \
 "src/display.obj" \
-"src/door_lock_system.obj" \
 "src/fsm.obj" \
 "src/joystick.obj" \
+"src/main.obj" \
 "src/push_button.obj" 
 
 C_DEPS__QUOTED += \
 "src/HAL_I2C.d" \
 "src/HAL_OPT3001.d" \
 "src/display.d" \
-"src/door_lock_system.d" \
 "src/fsm.d" \
 "src/joystick.d" \
+"src/main.d" \
 "src/push_button.d" 
 
 C_SRCS__QUOTED += \
 "../src/HAL_I2C.c" \
 "../src/HAL_OPT3001.c" \
 "../src/display.c" \
-"../src/door_lock_system.c" \
 "../src/fsm.c" \
 "../src/joystick.c" \
+"../src/main.c" \
 "../src/push_button.c" 
 
 
